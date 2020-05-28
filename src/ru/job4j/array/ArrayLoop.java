3a469ca2b1838e3607c926f4babbdb62d26897c8
mihanchik94 <mihanchik94@gmail.com>
2020-05-28 19:39:08 +0300

    6.0.3. Массивы и цикл for. [#294591]

---
+++ src/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] function = new int[5];
+        for (int index = 0; index < function.length; index++) {
+            function[index] = index * 2 + 3;
+        }
+            for (int index = 0; index < function.length; index++) {
+                System.out.println(function[index]);
+            }
+    }
+
+}
