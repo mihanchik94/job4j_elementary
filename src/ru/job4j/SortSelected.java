488e199158b71cb8a076be397f5f1bc48054bb6b
mihanchik94 <mihanchik94@gmail.com>
2020-06-02 16:33:39 +0300

    6.7.4. Выигрышные комбинации в сокобан [#294580]

--- src/ru/job4j/SortSelected.java
+++ src/ru/job4j/SortSelected.java
@@ -6,12 +6,23 @@ import ru.job4j.array.MinDiapason;
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-                int min = MinDiapason.findMin(data, i, data.length - 1);
-                int index = FindLoop.indexOf(data, min, 0, data.length - 1);
-                min = data[i];
-                data[i] = data[index];
-                data[index] = min;
-            }
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+            min = data[i];
+            data[i] = data[index];
+            data[index] = min;
+        }
         return data;
     }
+//    public static int[] sort(int[] data) {
+//        for (int i = 0; i < data.length; i++) {
+//            int min = MinDiapason.findMin(data, i, data.length - 1);
+//            int index = FindLoop.indexOf(data, min, 0, data.length - 1);
+//            data[index] = data[i];
+//            data[i] = min;
+//        }
+//        return data;
+//    }
+//    так можно еще
+//    можете закрывать
 }
