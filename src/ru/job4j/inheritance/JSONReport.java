87db825ed461f030c3d5cd26d36744801259c463
mihanchik94 <mihanchik94@gmail.com>
2020-06-17 15:41:34 +0300

    4. Переопределение [#294757]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,10 +1,13 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-    public static void main(String[] args) {
-        TextReport jsn = new TextReport();
-        String txt = jsn.generate("name : name", "body: body");
-        System.out.println(txt);
+    public String generate(String name, String body) {
+        return "name : " + name + System.lineSeparator() + "body : " + body;
+    }
 
+    public static void main(String[] args) {
+        JSONReport jsn = new JSONReport();
+        String txt = jsn.generate("name", "body");
+        System.out.println(txt);
     }
 }
