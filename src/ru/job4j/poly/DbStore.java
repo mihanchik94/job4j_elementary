d920e25a158e39b324339642eb4e1b827d72185f
mihanchik94 <mihanchik94@gmail.com>
2020-06-23 12:39:33 +0300

    Пример полиморфизма через интерфейс

---
+++ src/ru/job4j/poly/DbStore.java
@@ -0,0 +1,15 @@
+package ru.job4j.poly;
+
+public class DbStore implements Store {
+    public void save(String value) {
+    }
+
+
+    public void safe(String value) {
+
+    }
+
+    public String[] load() {
+        return new String[] {};
+    }
+}
---
+++ src/ru/job4j/poly/FileStore.java
@@ -0,0 +1,16 @@
+package ru.job4j.poly;
+
+public class FileStore implements Store {
+
+    public void save(String value) {
+    }
+
+
+    public void safe(String value) {
+
+    }
+
+    public String[] load() {
+        return new String[0];
+    }
+}
